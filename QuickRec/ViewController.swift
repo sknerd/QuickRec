51e4d61329df25e4b8d33bc9010853acfa5d11c8
renks <renksss@gmail.com>
2020-01-14 11:40:01 +0300

    Initial Commit

---
+++ QuickRec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickRec
+//
+//  Created by renks on 14.01.2020.
+//  Copyright © 2020 renks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
